f40d3da2a190f23d78790a92c05ba4862b69c32f
Pongsakorn <pongsakorn.t@kbtg.tech>
2021-09-06 14:51:06 +0700

    add employees

--- src/main/java/com/example/demoapp/employees/EmployeeController.java
+++ src/main/java/com/example/demoapp/employees/EmployeeController.java
@@ -8,6 +8,15 @@ public class EmployeeController {
 	
 	@GetMapping("/employees")
 	public EmployeeResponse[] listEmployees() {
-		return new EmployeeResponse[]{new EmployeeResponse()};
+		EmployeeResponse employee01 = new EmployeeResponse();
+		employee01.setId(1);
+		employee01.setName("Pongsakorn01");
+		
+		EmployeeResponse employee02 = new EmployeeResponse();
+		employee02.setId(2);
+		employee02.setName("Pongsakorn02");
+		
+		
+		return new EmployeeResponse[]{employee01, employee02};
 	}
 }
