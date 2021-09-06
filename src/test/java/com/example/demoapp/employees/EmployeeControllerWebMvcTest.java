54d6cf4252945a8b112495b286496b94a1d4333d
Pongsakorn <pongsakorn.t@kbtg.tech>
2021-09-06 16:24:20 +0700

    With working spring webmvc test

---
+++ src/test/java/com/example/demoapp/employees/EmployeeControllerWebMvcTest.java
@@ -0,0 +1,23 @@
+package com.example.demoapp.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(EmployeeController.class)
+public class EmployeeControllerWebMvcTest {
+	
+	@Autowired
+    private MockMvc mvc;
+	
+	@Test
+	public void getEmployeeById() throws Exception {
+//		mvc.perform(get("/employees/100")).andExpect(status().is(200));
+		mvc.perform(get("/employees/100")).andExpect(status().isOk());
+	}
+
+}
