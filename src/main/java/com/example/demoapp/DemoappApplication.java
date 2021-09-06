4a34cb212df9c12bdb7768a642f76eea9b3bb0ee
Pongsakorn <pongsakorn.t@kbtg.tech>
2021-09-06 14:16:14 +0700

    Count all beans

--- src/main/java/com/example/demoapp/DemoappApplication.java
+++ src/main/java/com/example/demoapp/DemoappApplication.java
@@ -1,13 +1,23 @@
 package com.example.demoapp;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 @SpringBootApplication
 public class DemoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoappApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DemoappApplication.class, args);
+		
+		String[] names = context.getBeanDefinitionNames();
+		Arrays.stream(names).forEach(System.out::println);
+		
+		System.out.println(context.getBeanDefinitionCount());
 	}
 
 }
