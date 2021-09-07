dd3af7f7da5dbc5c84f0e3f839dfa09f9812f2e0
Pongsakorn <pongsakorn.t@kbtg.tech>
2021-09-07 16:42:37 +0700

    Use rest template to call the external api

--- src/main/java/com/example/demoapp/users/UserGateway.java
+++ src/main/java/com/example/demoapp/users/UserGateway.java
@@ -1,11 +1,31 @@
 package com.example.demoapp.users;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class UserGateway {
 	
-	public UserResponse getUserById(int id) {
-		return null;
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public Optional<UserResponse> getUserById(int id) {
+		String url = "https://jsonplaceholder.typicode.com/users/" + id;
+//		UserResponse response = restTemplate.getForObject(url, UserResponse.class);
+//		if(response == null) {
+//			throw new RuntimeException("API error with id=" + id);
+//		}
+//		return response;
+		
+		try {
+			UserResponse result = restTemplate.getForObject(url, UserResponse.class);
+			return Optional.ofNullable(result);
+		}catch(RestClientException e) {
+			return Optional.empty();
+		}
 	}
 }
