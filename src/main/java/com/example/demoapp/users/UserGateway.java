a079d437193ca8bbdc87b13f3dd0b9297d39060b
Pongsakorn <pongsakorn.t@kbtg.tech>
2021-09-07 17:10:22 +0700

    Use application.properties to config domain of api

--- src/main/java/com/example/demoapp/users/UserGateway.java
+++ src/main/java/com/example/demoapp/users/UserGateway.java
@@ -3,6 +3,7 @@ package com.example.demoapp.users;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
@@ -11,15 +12,17 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class UserGateway {
 	
+	private final String apiHost;
 	private RestTemplate restTemplate;
 	
 	@Autowired
-	public UserGateway(RestTemplateBuilder builder) {
+	public UserGateway(RestTemplateBuilder builder, @Value("${external_api_url}") String apiHost) {
 		this.restTemplate = builder.build();
+		this.apiHost = apiHost;
 	}
 	
 	public Optional<UserResponse> getUserById(int id) {
-		String url = "https://jsonplaceholder.typicode.com/users/" + id;
+		String url = apiHost + "/users/" + id;
 //		UserResponse response = restTemplate.getForObject(url, UserResponse.class);
 //		if(response == null) {
 //			throw new RuntimeException("API error with id=" + id);
