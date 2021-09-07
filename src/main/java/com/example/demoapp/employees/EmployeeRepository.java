071756c404a9d141d13e034a60d16a1fa679e9c2
Pongsakorn <pongsakorn.t@kbtg.tech>
2021-09-07 10:13:00 +0700

    add test JPA case02

--- src/main/java/com/example/demoapp/employees/EmployeeRepository.java
+++ src/main/java/com/example/demoapp/employees/EmployeeRepository.java
@@ -1,7 +1,13 @@
 package com.example.demoapp.employees;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	
+//	List<Employee> findAllByIdAndName(int id,String name);
+	// select * from employee where id=? and name=?
 
+	
 }
