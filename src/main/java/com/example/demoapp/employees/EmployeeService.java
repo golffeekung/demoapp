f3da57c30f622a202fa2b3f687518ed80ad24e2f
Pongsakorn <pongsakorn.t@kbtg.tech>
2021-09-07 10:35:48 +0700

    add custom exception

--- src/main/java/com/example/demoapp/employees/EmployeeService.java
+++ src/main/java/com/example/demoapp/employees/EmployeeService.java
@@ -1,15 +1,29 @@
 package com.example.demoapp.employees;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
 	public EmployeeResponse getById(int id) {
-		EmployeeResponse response = new EmployeeResponse();
-		response.setId(id);
-		response.setName("Pongsakorn");
-		return response;
+//		EmployeeResponse response = new EmployeeResponse();
+//		response.setId(id);
+//		response.setName("Pongsakorn");
+		
+		Optional<Employee> result = employeeRepository.findById(id);
+		if(result.isPresent()) {
+			EmployeeResponse response = new EmployeeResponse();
+			response.setId(result.get().getId());
+			response.setName(result.get().getName());
+			return response;
+		}
+		throw new EmployeeNotFoundException("Employee not found id=" + id);
 	}
 	
 }
