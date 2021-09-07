06229db69956143ca3a1dce12210854a9c8ae4c4
Pongsakorn <pongsakorn.t@kbtg.tech>
2021-09-07 13:58:24 +0700

    Fix with fail case

--- src/test/java/com/example/demoapp/employees/EmployeeControllerServiceTest.java
+++ src/test/java/com/example/demoapp/employees/EmployeeControllerServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.demoapp.employees;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerServiceTest {
@@ -39,5 +41,21 @@ public class EmployeeControllerServiceTest {
 		// Assert
 		assertEquals(id , result.getId());
 		assertEquals("Pongsakorn" , result.getName());
+	}
+	
+	@Test
+	@DisplayName("Failure case :: Employee not found id = 100")
+	public void case02() {
+		int id = 100;
+		Employee employee = new Employee();
+		employee.setId(1);
+		when(employeeRepository.findById(100)).thenReturn(Optional.empty());
+		// Act
+		ResponseEntity<ErrorResponse> result = restTemplate.getForEntity("/employees/" + id, ErrorResponse.class);
+		
+		// Assert
+		assertEquals(404 , result.getStatusCodeValue());
+		assertEquals(404, result.getBody().getCode());
+		assertEquals("Employee not found id=100", result.getBody().getDetail());
 	}
 }
